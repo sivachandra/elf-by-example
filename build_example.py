bfe19b86dfb508f3f42aa4efe5006a7edf56330b
Siva Chandra Reddy <sivachandra@google.com>
2019-04-11 15:02:12 -0700

    Use compiler and linker from the llvm-project from the sub-module.

--- build_example.py
+++ build_example.py
@@ -6,6 +6,16 @@ import os
 import muslflex_utils
 
 
+_COMPILERS = [
+    os.path.join(muslflex_utils.LLVM_BUILD_DIR, "bin", "clang"),
+    # Should we add GCC here?
+]
+
+_LINKERS = [
+    "ld",  # We will assume that the BFD linker is available in path as "ld".
+    os.path.join(muslflex_utils.LLVM_BUILD_DIR, "bin", "ld.lld"),
+]
+
 def _get_install_dir(libc):
   if libc == "glibc":
     return muslflex_utils.GLIBC_INSTALL_DIR
@@ -42,40 +52,42 @@ def _parse_args():
   parser.add_argument(
       "--example", "-p", dest="example", type=str, required=True,
       help="Path to the example directory")
-  parser.add_argument(
-      "--compiler", "-C", dest="compiler", type=str, required=True,
-      help="Path to the compiler executable.")
-  parser.add_argument(
-      "--linker", "-L", dest="linker", type=str, required=True,
-      help="Path to the linker executable.")
   return parser.parse_args()
 
 
-def _build(compiler, linker, src_file, output_name):
-  compiler_suffix = os.path.basename(compiler)
-  linker_suffix = os.path.basename(linker)
-  for libc in ("glibc", "musl"):
-    object_file = output_name + "." + libc + ".o"
-    compile_cmd = [compiler,
-                   "-nostdinc",
-                   "-I" + _get_include_dir(libc),
-                   "-I" + os.path.join(muslflex_utils.GCC_INSTALL_DIR,
-                                       "lib/gcc/x86_64-pc-linux-gnu/9.0.1/include/"),
-                   "-fPIC",
-                   "-o", object_file, "-g", "-O0", "-c", src_file]
-    muslflex_utils.run_step(name="Compiling %s" % object_file, cmd=compile_cmd)
-
-    exe_file = ".".join([output_name, compiler_suffix, linker_suffix, libc])
-    link_cmd = [linker, "-nostdlib",
-                "-static",
-                "-pie", "--no-dynamic-linker",
-                "-L" + _get_lib_dir(libc), "-L" + muslflex_utils.GCC_LIB_DIR,
-                object_file, _get_rcrt1(libc), _get_crti(libc),
-                muslflex_utils.GCC_CRT_BEGIN,
-                "--start-group", "-lc", "-lgcc", "-lgcc_eh", "--end-group",
-                muslflex_utils.GCC_CRT_END, _get_crtn(libc),
-                "-o", exe_file]
-    muslflex_utils.run_step(name="Linking %s" % exe_file, cmd=link_cmd)
+def _build(src_file, output_name):
+  for compiler in _COMPILERS:
+    compiler_suffix = os.path.basename(compiler)
+    for libc in ("glibc", "musl"):
+      object_file = ".".join([output_name, compiler_suffix, libc, "o"])
+      compile_cmd = [compiler,
+                     "-nostdinc",
+                     "-I" + _get_include_dir(libc),
+                     "-I" + os.path.join(
+                         muslflex_utils.GCC_INSTALL_DIR,
+                         "lib/gcc/x86_64-pc-linux-gnu/9.0.1/include/"),
+                     "-fPIC",
+                     "-o", object_file, "-g", "-O0", "-c", src_file]
+      muslflex_utils.run_step(name="Compiling %s" % object_file,
+                              cmd=compile_cmd)
+    
+    for linker in _LINKERS:
+      linker_suffix = os.path.basename(linker)
+      for libc in ("glibc", "musl"):
+        object_file = ".".join([output_name, compiler_suffix, libc, "o"])
+        exe_file = object_file[:-1] + linker_suffix  # Remove .o and
+                                                     # add .<linker name>
+        link_cmd = [linker, "-nostdlib",
+                    "-static",
+                    "-pie", "--no-dynamic-linker",
+                    "-L" + _get_lib_dir(libc),
+                    "-L" + muslflex_utils.GCC_LIB_DIR,
+                    object_file, _get_rcrt1(libc), _get_crti(libc),
+                    muslflex_utils.GCC_CRT_BEGIN,
+                    "--start-group", "-lc", "-lgcc", "-lgcc_eh", "--end-group",
+                    muslflex_utils.GCC_CRT_END, _get_crtn(libc),
+                    "-o", exe_file]
+        muslflex_utils.run_step(name="Linking %s" % exe_file, cmd=link_cmd)
 
 
 def main():
@@ -86,7 +98,7 @@ def main():
   basename = os.path.basename(example_path)
   src_file = os.path.join(os.path.abspath(example_path), "main.c")
   output_name = os.path.join(muslflex_utils.OUT_DIR, example_path, basename)
-  _build(args.compiler, args.linker, src_file, output_name)
+  _build(src_file, output_name)
   return 0
   
 
